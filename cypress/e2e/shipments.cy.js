05d2bac8bd3112b02d9c055155fb9ae1df70bc38
Kilian Melcher <kilian.melcher@vtex.com>
2024-04-16 09:09:29 -0300

    Add 3 cypress tests cases for shippment

--- cypress/e2e/shipments.cy.js
+++ cypress/e2e/shipments.cy.js
@@ -4,11 +4,10 @@ describe('shipments', () => {
     cy.get('[id="_username"]').type('sylius');
     cy.get('[id="_password"]').type('sylius');
     cy.get('.primary').click();
-  });
-  // Remove .only and implement others test cases!
-  it.only('ship a ready shipment', () => {
-    // Click in shipments in side menu
     cy.clickInFirst('a[href="/admin/shipments/"]');
+  });
+
+  it('ships a ready shipment', () => {
     // Type in value input to search for specify shipment
     cy.get('.ui > .sylius-filters > .sylius-filters__field > .field > #criteria_state').select('ready');
     // Click in filter blue button
@@ -19,11 +18,35 @@ describe('shipments', () => {
     // Assert that shipment has been shipped
     cy.get('body').should('contain', 'Shipment has been successfully shipped.');
   });
-  it('test case 2', () => {
-    // Implement your test case 2 code here
+
+  it('shows only shipped shipments by selecting shipped in state filter', () => {
+    cy.get('#criteria_state').select('Shipped');
+    cy.get('button').contains('Filter').click();
+
+    cy.get('.item').contains('Shipped').should('be.visible');
+    cy.get('.item').should('not.contain.text', 'Cancelled');
+    cy.get('.item').should('not.contain.text', 'Ready');
   });
-  it('test case 3', () => {
-    // Implement your test case 3 code here
+
+  it('shows only ready shipments by selecting ready in state filter', () => {
+    cy.get('#criteria_state').select('Ready');
+    cy.get('button').contains('Filter').click();
+
+    cy.get('.item').contains('Ready').should('be.visible');
+    cy.get('.item').should('not.contain.text', 'Cancelled');
+    cy.get('.item').should('not.contain.text', 'Shipped');
+  });
+
+  it('should remove all filters by clicking clear filters button', () => {
+    cy.get('#criteria_state').select('Ready');
+    cy.get('#criteria_channel').select('Home Plus Web Store');
+    cy.get('#criteria_method').select('UPS');
+
+    cy.get('.button').contains('Clear filters').click();
+
+    cy.get('#criteria_state').should('contain.text', 'All');
+    cy.get('#criteria_channel').should('contain.text', 'All');
+    cy.get('#criteria_method').should('contain.text', 'All');
   });
 
   // Implement the remaining test cases in a similar manner
